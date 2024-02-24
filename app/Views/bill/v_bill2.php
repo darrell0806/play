baec38454bd541fb9e338c7f1089cc85091404f8
unknown <darrell260806@gmail.com>
2024-02-24 21:46:34 +0700

    Darrell

--- app/Views/bill/v_bill2.php
+++ app/Views/bill/v_bill2.php
@@ -1,3 +1,6 @@
+<?php
+if(session()->get('level')== 1){
+	?>
 <div id="main-content">
   <div class="page-heading">
     <div class="page-title">
@@ -72,3 +75,79 @@
     </section>
     <!-- Basic Tables end -->
   </div>
+	<?php
+								}else if(session()->get('level')== 2){
+									?>
+<div id="main-content">
+  <div class="page-heading">
+    <div class="page-title">
+      <div class="row">
+        <div class="col-12 col-md-6 order-md-1 order-last">
+          <h3><?=$title?></h3>
+          <p class="text-subtitle text-muted">Anda dapat melihat <?=$title?> di bawah</p>
+        </div>
+        <div class="col-12 col-md-6 order-md-2 order-first">
+          <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="<?=base_url('dashboard')?>">Dashboard</a></li>
+              <li class="breadcrumb-item active" aria-current="page"><?=$title?></li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <!-- Basic Tables start -->
+    <section class="section">
+      <div class="card">
+        <div class="card-header">
+          <a href="<?=base_url('/bill2/tambah/')?>">
+            <button class="btn btn-primary"><i class="fa-solid fa-plus"></i>
+            Tambah</button>
+          </a>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table" id="table1">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama</th>
+                  <th>Harga</th>
+                  <th>Jenis</th>
+                  <th>Menit</th>
+                  <th>Status</th>
+                  <th>Jam Masuk</th>
+                  <th>Jam Keluar</th>
+                
+                </tr>
+              </thead>
+              <tbody>
+                <?php 
+                $no=1;
+                foreach ($a as $b) {
+                  ?>
+                  <tr>
+                    <td><?php echo $no++ ?></td>
+                    <td><?php echo $b->nama?> </td>
+                    <td><?php echo $b->harga?> </td>
+                    <td><?php echo $b->nama_jenis?> </td>
+                    <td><?php echo $b->menit?> </td>
+                    <td><?php echo $b->status?> </td>
+                    <td><?php echo $b->jam_m?> </td>
+                    <td><?php echo $b->jam_k?> </td>
+                  
+
+                  </tr>
+                  <?php
+                }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Basic Tables end -->
+  </div>
+	<?php } ?>
